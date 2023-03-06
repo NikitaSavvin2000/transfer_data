bc48935d26030b76a1b652ed295e89baa9b508d2
Savvin Nikita Vladimirovich <savvin.nikita.1job@yandex.ru>
2023-03-06 10:49:51 +0300

    In the def dates added an exception case when table empty

--- filling.py
+++ filling.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 
 def dates():
-    date_test = "03.05.2022" # Pull up last date from general table
-    last_value_date = dt.strptime(date_test, "%d.%m.%Y")
+    last_date_table = "05.03.2022" # Pull up last date from general table
+    #last_date_table = None # For test if table empty
+    if last_date_table is None:
+        last_date_table = "04.03.2023" # When run code first time for fresh, empty table, last_date_table = = 01.01.2016
+    last_value_date = dt.strptime(last_date_table, "%d.%m.%Y")
     date_now = dt.now()
     date_today = dt(year=date_now.year, month=date_now.month, day=date_now.day)
     date_yesterday = date_today - td(days=1) # Using yesyerday date because all data
@@ -19,3 +22,5 @@ def create_date_df(last_value_date, date_yesterday):
 
 last_value_date, date_yesterday = dates()
 df = create_date_df(last_value_date, date_yesterday)
+print(df)
+
